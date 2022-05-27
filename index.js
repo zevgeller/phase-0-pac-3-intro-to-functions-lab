cf086941e8aa45c79b4adf662bce00a16d9e16dd
Zev <zevgeller@gmail.com>
2022-05-27 02:43:17 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string)  {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    const spy = string.toUpperCase();
+    console.log(spy)
+}
+
+function logWhisper(string) {
+    const spy = string.toLowerCase();
+    console.log(spy)
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if(string === string.toLowerCase()) {
+        return `I can\'t hear you!`
+    } 
+    else if(string === string.toUpperCase()) {
+        return "YES INDEED!"
+    }
+    else if(string === `Let\'s have dinner together!`) {
+        return "I would love to!"
+    }
+}
+sayHiToHeadphonedRoommate()
